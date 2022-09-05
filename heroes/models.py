8644472c4fe17d32dea512a605211c8e89f82651
Yowill89 <guillermo.oscar.acuna@gmail.com>
2022-09-05 09:30:19 -0300

    App heroes agregada. Primera migracin de heroes app

--- heroes/models.py
+++ heroes/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class superHeroe(models.Model):
+class superHeroes(models.Model):
     nombre = models.CharField(max_length=128)
     fecha_nacimiento = models.DateField()
     peso_kg = models.IntegerField()
     
 
-class Poder(models.models):
+class Poder(models.Model):
     nombre = models.CharField(max_length=128)
     super_heroes = models.ForeignKey('superHeroes',on_delete=models.CASCADE)
-    
